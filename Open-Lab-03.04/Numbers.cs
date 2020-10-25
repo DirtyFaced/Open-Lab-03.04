d5322483a24a3089b0da2228a91368e93239c7a6
Jakub Dipčár <jakub.dipcar@gmail.com>
2020-10-25 14:33:16 +0100

    chlp

--- Open-Lab-03.04/Numbers.cs
+++ Open-Lab-03.04/Numbers.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_03._04
     {
         public string EvenOrOdd(int number)
         {
-            throw new NotImplementedException();
+            string vysledok = "odd";
+            if (number % 2 == 0)
+            { vysledok = "even"; };
+            return vysledok;
         }
     }
 }
